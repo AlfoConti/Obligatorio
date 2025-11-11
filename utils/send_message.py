bba0d1ef7ac31db64b84d36e1ec8cb5f16bc283a
AlfoConti <alfocontii@gmail.com>
2025-11-10 21:40:07 -0300

    probando que pepito come papas fritas

--- utils/send_message.py
+++ utils/send_message.py
@@ -14,8 +14,8 @@ def generate_data(text_user, number_user):
 # LÍNEA 79
 def send_message_whatsapp(text_user, number_user):
     try:
-        token = "EAALZCWMF3l0cBP4ZBZCUAZBaHpco2fgDuX76oZCKiEmTFjROjRuV0ZB8rVPkFq9hWkOYgrTzZAr4vx5nQXiDq0YyVt6JrF7qiC6wxFiTHrZB8MF6NpVyFKZC99N1i2w2zZAtYpu6QNxv8lTGTDzDFnZBZC9ZAHZAzB22lgSP4c7omSsNUwYiqN1G6YbMDyAZArSxZAYFgQZDZD" # Obtener de variables de entorno o configuración segura
-        api_url = "https://graph.facebook.com/v22.0/846928765173274/messages" # Obtener de variables de entorno o configuración segura
+        token = "" # Obtener de variables de entorno o configuración segura
+        api_url = "" # Obtener de variables de entorno o configuración segura
         data = generate_data(text_user, number_user)
         headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
         
